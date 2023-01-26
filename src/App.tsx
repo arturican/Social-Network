d2b510e8c9384287ed1c4d7e76b13a51a29f614e
arturican <arturican@gmail.com>
2023-01-26 13:44:54 +0300

    исправил роуты

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,22 @@ import {Header} from "./components/Header/Header";
 import {Sidebar} from "./components/Sidebar/Sidebar";
 import {Profile} from "./components/Profile/Profile";
 import {About} from "./components/About/About";
-import {BrowserRouter,  Route, Routes,} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes,} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Sidebar/>
-            <div className='App-content'>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path='/' element={<About/>}/>
-                        <Route path='about' element={<About/>}/>
-                        <Route path='post' element={<Profile/>}/>
-                    </Routes>
-
-                </BrowserRouter>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Sidebar/>
+                <div className='App-content'>
+                    <Route path='/' element={<About/>}/>
+                    <Route path='about' element={<About/>}/>
+                    <Route path='post' element={<Profile/>}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
+
     );
 }
 
