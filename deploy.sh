d2ecda13c2476fe58b585ebce962dff082676893
arturican <arturican@gmail.com>
2023-07-04 09:35:55 +0300

    переехал на новый сервак

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 yarn run build
 
 echo "Deploy files to server..."
-scp -r build/* art@167.99.136.103:/var/www/167.99.136.103/
+scp -r build/* root@194.28.224.100:/var/www/194.28.224.100
 
 echo "Done!"
